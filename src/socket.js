a2eb10a9bd4ab230f3c4edca16d1b5836a6f23c8
Sudhakar0512 <sudhakar.s2021csbs@sece.ac.in>
2024-06-27 21:56:55 +0530

    updated

--- src/socket.js
+++ src/socket.js
@@ -1,26 +1,12 @@
-// import { io } from 'socket.io-client';
-
-// export const initSocket = async () => {
-//     const options = {
-//         'force new connection': true,
-//         reconnectionAttempt: 'Infinity',
-//         timeout: 10000,
-//         transports: ['websocket'],
-//     };
-//     // return io(process.env.REACT_APP_BACKEND_URL, options);
-//     return io("/", options);
-// };
-
-
 import { io } from 'socket.io-client';
 
 export const initSocket = async () => {
     const options = {
         'force new connection': true,
-        reconnectionAttempts: 'Infinity',
+        reconnectionAttempt: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
     };
-    console.log('Connecting to:', process.env.REACT_APP_BACKEND_URL); // Debugging log
     return io(process.env.REACT_APP_BACKEND_URL, options);
+    // return io("/", options);
 };
